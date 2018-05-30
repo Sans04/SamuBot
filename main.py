b73eb3d672726e91a6ae78d020f1e87e5bc550c8
Sans04 <39777638+Sans04@users.noreply.github.com>
2018-05-31 00:02:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----------')
-    await client.change_presence(game=discord.Game(name="SansBar" + "<:yay:450746929702371328>"))
+    await client.change_presence(game=discord.Game(name="SansBar 🔥"))
 
 
 @client.event
