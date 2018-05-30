61fce97051ed001a94aa361deb3bfbea1156120e
Sans04 <39777638+Sans04@users.noreply.github.com>
2018-05-30 23:10:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,47 +38,7 @@ async def on_message(message):
     if message.content.startswith('?uptime'):
         await client.send_message(message.channel, "`Ich bin schon {0} stunde/n und {1} minuten online auf {2}. `".format(hour, minutes, message.server))
 
-    if message.content.lower().startswith('?embed'):
-        embed = discord.Embed(
-            title="Hallo World",
-            color=0xe67e22,
-            description="guten tag welt adjkhfg iadsfgjkadsh gjkahdfbg adfg\n"
-                        "yhuasdghasd\n"
-                        "guten\n"
-                        "tag"
-        )
-        embed.set_author(
-            name="Bobo",
-            icon_url="https://cdn.pixabay.com/photo/2017/05/16/21/24/gorilla-2318998_960_720.jpg",
-            url="http://grewoss.com/"
-        )
-        embed.add_field(
-            name="Title 2",
-            value="Description 2\n"
-                  "fgasdfujhkasfdgk adfg da\n"
-                  "asdgufhadsfjgkhads\n"
-                  "afsdgadifghahdioufg\n",
-            inline=False
-        )
-        embed.add_field(
-            name="haladfgad",
-            value="asdgasdfg",
-            inline=False
-        )
-        embed.add_field(
-            name="asdggasdfgadfhg",
-            value="adfiohgjadfi giuhadf gadjkfhg2222",
-            inline=False
-        )
-        embed.set_footer(
-            text="Ein bot von mir",
-            icon_url="https://cdn.pixabay.com/photo/2017/05/16/21/24/gorilla-2318998_960_720.jpg"
-        )
-        embed.set_thumbnail(
-            url="https://cdn.pixabay.com/photo/2017/05/16/21/24/gorilla-2318998_960_720.jpg"
-        )
 
-        await client.send_message(message.channel, embed=embed)
         
 async def tutorial_uptime():
     await client.wait_until_ready()
