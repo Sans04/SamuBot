0bee5877ba103720fdd62221233f9865e2be5743
Sans04 <39777638+Sans04@users.noreply.github.com>
2018-05-30 22:30:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,6 @@ async def on_message(message):
         await client.send_message(message.channel, "Test bestanden")
     if message.content.lower().startswith('hi'):
         await client.send_message(message.channel, "heyy")
-    if message.author.name == "alex-botxd":
-    if message.content.lower().startswith("Witz des Tages"):
-        await client.add_reaction(botmsg, "👍")
-        await client.send_message(message.channel, "LUSTIG")
-        asyncio.sleep(2)
-       
-        await client.send_message(message.channel, "NICHT" + ":yay:")
-        
-        global testmsgid
-        testmsgid = botmsg.id
-
-        global testmsguser
-        testmsguser = message.author
 
  
     if message.content.startswith('?uptime'):
